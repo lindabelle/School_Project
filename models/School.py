7ebb0f12cca61e0718291b563709066ad6a9e78b
lindabelle <alexxllxx1@gmail.com>
2022-02-20 17:45:07 +0300

    changes add student and delete student were added

--- models/School.py
+++ models/School.py
@@ -2,8 +2,9 @@ from models.Subject import Subject
 from models.Teacher import Teacher
 from models.Student import Student
 
+
 class School:
-    def __init__(self, subjects_name:list, password:str):
+    def __init__(self, subjects_name: list, password: str):
         self.__subjects = {}
         for name in subjects_name:
             tmp_subject = Subject(name)
@@ -12,30 +13,27 @@ class School:
         self.__students = {}
         self.__password = password
 
-    def check_password(self,password):
+    def check_password(self, password):
         if password == self.__password:
             return True
         return False
 
-    def add_teacher(self,surname: str, firstname: str, lastname: str, subject: Subject):
-        new_teacher = Teacher(surname,firstname,lastname,subject)
-        self.__teachers.update({new_teacher.teacher_number:new_teacher})
+    def add_teacher(self, surname: str, firstname: str, lastname: str, subject: Subject):
+        new_teacher = Teacher(surname, firstname, lastname, subject)
+        self.__teachers.update({new_teacher.teacher_number: new_teacher})
 
-    def delete_teacher(self,teacher_number):
+    def delete_teacher(self, teacher_number):
         self.__teachers.pop(teacher_number)
 
-    def get_teacher(self,teacher_number):
+    def get_teacher(self, teacher_number):
         return self.__teachers[teacher_number]
 
+    def add_student(self, surname: str, firstname: str, lastname: str, grade: int):
+        new_student = Student(surname, firstname, lastname, grade)
+        self.__students.update({new_student.student_number: new_student})
+
+    def delete_student(self, student_number):
+        self.__students.pop(student_number)
 
 
-
-
-
-
-
-a = Subject('russian')
-b = Subject('eng')
-name = 'math'
-c = Subject(name)
 
